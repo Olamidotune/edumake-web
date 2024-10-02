ccf81d4b7e270c09c6a615504f3d66c606273ce2
olamidotune <davidegundeyi@gmail.com>
2024-10-02 04:34:48 +0100

    chore: started the onboarding ui

--- lib/src/features/onboarding/presentation/pages/onboarding_screen.dart
+++ lib/src/features/onboarding/presentation/pages/onboarding_screen.dart
@@ -1,4 +1,7 @@
+import 'package:edumake_frontend/src/core/constants/app_spacing.dart';
+import 'package:edumake_frontend/src/core/extentions/num_extention.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -7,11 +10,39 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Onboarding'),
-      ),
-      body: const Center(
-        child: Text('Welcome to the Onboarding Screen!'),
+      body: Padding(
+        padding: EdgeInsets.all(AppSpacing.horizontalSpacing),
+        child: Column(
+          children: [
+            SizedBox(
+              height: .10.sh,
+            ),
+            Stack(
+              children: [
+                Image.asset(
+                  'assets/png/onboarding_background.png',
+                ),
+                Positioned(
+                  top: .17.sh,
+                  right: 01.width,
+                  left: 01.width,
+                  child: Image.asset(
+                    'assets/png/onboarding.png',
+                    height: 140.height,
+                  ),
+                ),
+              ],
+            ),
+            AppSpacing.verticalSpaceHuge,
+            Text(
+              "Welcome to Edu-Make, your one-stop portal for staying informed about your child's academic journey. Access real-time updates on grades, attendance, assignments, and more. Easily connect with teachers and parents to support student progress as a school.",
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    fontSize: 16.fontSize,
+                  ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
