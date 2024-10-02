2aca8ce969eba306fad2596f5c845202450071eb
olamidotune <davidegundeyi@gmail.com>
2024-10-02 02:45:24 +0100

    chore: added app colors

---
+++ lib/src/core/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static const primaryColor = Color(0xFF004186);
+  static const secondaryColor = Color(0xFF004186);
+  static const greenColor = Color(0xFF34C759);
+  static const redColor = Color(0xFFFF3B30);
+  static const primaryTextColor = Color(0xFF171717);
+  static const secondaryTexColor = Color(0xFF373737);
+}
