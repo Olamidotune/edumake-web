ba51165d3702474cd80c5f6d351622086e503aa7
olamidotune <davidegundeyi@gmail.com>
2024-10-07 19:15:26 +0100

    feat: compelted the small social button

--- lib/src/features/authentication/presentation/pages/sign_in.dart
+++ lib/src/features/authentication/presentation/pages/sign_in.dart
@@ -5,10 +5,10 @@ import 'package:edumake_frontend/src/features/authentication/presentation/pages/
 import 'package:edumake_frontend/src/shared/widgets/app_bar.dart';
 import 'package:edumake_frontend/src/shared/widgets/button.dart';
 import 'package:edumake_frontend/src/shared/widgets/custom_text_form_field.dart';
+import 'package:edumake_frontend/src/shared/widgets/small_social_button.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_svg/svg.dart';
 
 class SignIn extends HookWidget {
   const SignIn({super.key});
@@ -228,42 +228,6 @@ class SignIn extends HookWidget {
               AppSpacing.verticalSpaceMassive,
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class SmallSocialButton extends StatelessWidget {
-  const SmallSocialButton({
-    required this.icon,
-    super.key,
-    this.onPressed,
-  });
-  final String icon;
-  final void Function()? onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 70,
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: ButtonStyle(
-          elevation: WidgetStateProperty.all<double>(10),
-          enableFeedback: true,
-          backgroundColor: WidgetStateProperty.all<Color>(
-            AppColors.whiteColor,
-          ),
-          shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-              side: const BorderSide(color: AppColors.whiteColor),
-              borderRadius: BorderRadius.circular(20.radius),
-            ),
-          ),
-        ),
-        child: SvgPicture.asset(
-          'assets/svg/$icon.svg',
         ),
       ),
     );
