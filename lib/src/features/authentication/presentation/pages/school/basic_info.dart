dc522b531b537c617a01626adf637011666c905c
olamidotune <davidegundeyi@gmail.com>
2024-10-31 13:03:56 +0100

    refactor: refactored code

--- lib/src/features/authentication/presentation/pages/school/basic_info.dart
+++ lib/src/features/authentication/presentation/pages/school/basic_info.dart
@@ -1,6 +1,7 @@
 import 'package:edumake_frontend/src/core/constants/app_colors.dart';
 import 'package:edumake_frontend/src/core/constants/app_spacing.dart';
 import 'package:edumake_frontend/src/core/extentions/num_extention.dart';
+import 'package:edumake_frontend/src/features/authentication/presentation/pages/school/widgets/school_drop_down_form.dart';
 import 'package:edumake_frontend/src/features/dashboard/presentation/pages/dashboard.dart';
 import 'package:edumake_frontend/src/shared/widgets/app_bar.dart';
 import 'package:edumake_frontend/src/shared/widgets/button.dart';
@@ -265,68 +266,6 @@ class SchoolBasicInfoScreen extends HookWidget {
               ],
             ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class SchoolDropDownFormWidget extends StatelessWidget {
-  const SchoolDropDownFormWidget({
-    required this.hintText,
-    super.key,
-  });
-  final String hintText;
-
-  @override
-  Widget build(BuildContext context) {
-    final l10n = AppLocalizations.of(context)!;
-    return Theme(
-      data: Theme.of(context).copyWith(
-        scrollbarTheme: ScrollbarThemeData(
-          thumbColor: MaterialStateProperty.all(AppColors.primaryColor),
-          thickness: MaterialStateProperty.all(6),
-          radius: const Radius.circular(8),
-          thumbVisibility: MaterialStateProperty.all(
-            true,
-          ),
-        ),
-      ),
-      child: ScrollConfiguration(
-        behavior: ScrollConfiguration.of(context).copyWith(
-          scrollbars: true,
-          physics: const BouncingScrollPhysics(),
-        ),
-        child: DropdownButtonFormField<String>(
-          isExpanded: true,
-          itemHeight: 64,
-          menuMaxHeight: 200,
-          hint: Text(hintText),
-          validator: (value) {
-            if (value == null) {
-              return l10n.fieldIsRequired;
-            }
-            return null;
-          },
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: AppColors.greyColor.withOpacity(0.1),
-            border: OutlineInputBorder(
-              borderSide: BorderSide.none,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            contentPadding: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 8,
-            ),
-          ),
-          items: List.generate(15, (index) {
-            return DropdownMenuItem(
-              value: 'Item ${index + 1}',
-              child: Text('Item ${index + 1}'),
-            );
-          }),
-          onChanged: print,
         ),
       ),
     );
---
+++ lib/src/features/authentication/presentation/pages/school/widgets/school_drop_down_form.dart
@@ -0,0 +1,67 @@
+
+import 'package:edumake_frontend/src/core/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+
+class SchoolDropDownFormWidget extends StatelessWidget {
+  const SchoolDropDownFormWidget({
+    required this.hintText,
+    super.key,
+  });
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+    return Theme(
+      data: Theme.of(context).copyWith(
+        scrollbarTheme: ScrollbarThemeData(
+          thumbColor: MaterialStateProperty.all(AppColors.primaryColor),
+          thickness: MaterialStateProperty.all(6),
+          radius: const Radius.circular(8),
+          thumbVisibility: MaterialStateProperty.all(
+            true,
+          ),
+        ),
+      ),
+      child: ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(
+          scrollbars: true,
+          physics: const BouncingScrollPhysics(),
+        ),
+        child: DropdownButtonFormField<String>(
+          isExpanded: true,
+          itemHeight: 64,
+          menuMaxHeight: 200,
+          hint: Text(hintText),
+          validator: (value) {
+            if (value == null) {
+              return l10n.fieldIsRequired;
+            }
+            return null;
+          },
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: AppColors.greyColor.withOpacity(0.1),
+            border: OutlineInputBorder(
+              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+          ),
+          items: List.generate(15, (index) {
+            return DropdownMenuItem(
+              value: 'Item ${index + 1}',
+              child: Text('Item ${index + 1}'),
+            );
+          }),
+          onChanged: print,
+        ),
+      ),
+    );
+  }
+}
