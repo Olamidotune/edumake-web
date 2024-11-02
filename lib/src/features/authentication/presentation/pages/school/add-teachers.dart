9f689d52b1bec98ac7124275c3ead75a284485fe
olamidotune <davidegundeyi@gmail.com>
2024-11-02 23:59:54 +0100

    feat: created the add teacher screen

---
+++ lib/src/features/authentication/presentation/pages/school/add-teachers.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddTeachersScreen extends StatefulWidget {
+  const AddTeachersScreen({super.key});
+
+  static const String routeName = '/add-teachers';
+
+  @override
+  State<AddTeachersScreen> createState() => _AddTeachersScreenState();
+}
+
+class _AddTeachersScreenState extends State<AddTeachersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
