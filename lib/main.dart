27f03bd65fb26f3473c52bb425d1f1041eeb8375
olamidotune <davidegundeyi@gmail.com>
2024-11-18 01:41:34 +0100

    feat: update home screen to use SplashScreen instead of SchoolBasicInfoScreen

--- lib/main.dart
+++ lib/main.dart
@@ -107,7 +107,7 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
-              home: const SchoolBasicInfoScreen(),
+              home: const SplashScreen(),
               routes: {
                 SplashScreen.routeName: (context) => const SplashScreen(),
                 SelectLanguageScreen.routeName: (context) =>
