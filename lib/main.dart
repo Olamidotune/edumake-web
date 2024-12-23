6ee4b4077309dc89ef6dea6a159507ba4b2502f5
olamidotune <davidegundeyi@gmail.com>
2024-12-23 10:44:35 +0100

    feat: add SchoolMenuScreen and update imports in payment_screen and main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,8 @@ import 'package:edumake_frontend/src/features/dashboard/presentation/pages/dashb
 import 'package:edumake_frontend/src/features/dashboard/presentation/pages/tabs/home_screen.dart';
 import 'package:edumake_frontend/src/features/dashboard/presentation/pages/tabs/settings_screen.dart';
 import 'package:edumake_frontend/src/features/dashboard/presentation/pages/tabs/users_home_screens/school_home_screens.dart';
-import 'package:edumake_frontend/src/features/dashboard/presentation/pages/tabs/users_payment_screens/classes_screen.dart';
+import 'package:edumake_frontend/src/features/dashboard/presentation/pages/tabs/users_payment_screens/school_classes_screen.dart';
+import 'package:edumake_frontend/src/features/dashboard/presentation/pages/tabs/users_settings_screens/school_menu_screens.dart';
 import 'package:edumake_frontend/src/features/dashboard/presentation/pages/tabs/users_ward_screens/school_teacher_screen.dart';
 import 'package:edumake_frontend/src/features/dashboard/presentation/pages/tabs/ward_screen.dart';
 import 'package:edumake_frontend/src/features/onboarding/presentation/pages/onboarding_screen.dart';
@@ -171,6 +172,8 @@ class MyApp extends StatelessWidget {
                     const ClassEventDetailsScreen(),
                 AddEventsScreen.routeName: (context) =>
                     const AddEventsScreen(),
+                SchoolMenuScreen.routeName: (context) =>
+                    const SchoolMenuScreen(),
                 VerifyAccount.routeName: (context) => const VerifyAccount(),
                 Dashboard.routeName: (context) => const Dashboard(),
                 KycScreen.routeName: (context) => const KycScreen(),
---
+++ lib/src/features/dashboard/presentation/pages/tabs/users_settings_screens/school_menu_screens.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SchoolMenuScreen extends StatelessWidget {
+  const SchoolMenuScreen({super.key});
+
+  static const String routeName = '/school-menu';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
