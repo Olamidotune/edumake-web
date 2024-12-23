58a0128494b18be0e6cbaaeaabf8dd0769d656c6
olamidotune <davidegundeyi@gmail.com>
2024-12-23 15:30:28 +0100

    feat: change home screen from Dashboard to SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -111,7 +111,7 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
-              home: const Dashboard(),
+              home: const SplashScreen(),
               routes: {
                 SplashScreen.routeName: (context) => const SplashScreen(),
                 SelectLanguageScreen.routeName: (context) =>
