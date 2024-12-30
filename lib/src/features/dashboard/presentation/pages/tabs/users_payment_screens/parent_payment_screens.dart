0d7e3b0cea7c9c7eb278013fb72d2131774f285d
Olamidotune <davidegundeyi@gmail.com>
2024-12-30 19:14:02 +0100

    feat: enhance ParentPaymentScreen with school fee payment details and layout improvements

--- lib/src/features/dashboard/presentation/pages/tabs/users_payment_screens/parent_payment_screens.dart
+++ lib/src/features/dashboard/presentation/pages/tabs/users_payment_screens/parent_payment_screens.dart
@@ -247,6 +247,288 @@ class ParentPaymentScreen extends StatelessWidget {
             ],
           ),
         ),
+        AppSpacing.verticalSpaceMedium,
+        Container(
+          padding: EdgeInsets.all(AppSpacing.horizontalSpacing),
+          decoration: BoxDecoration(
+            color: AppColors.primaryColor.withOpacity(0.1),
+            border: Border.all(
+              color: AppColors.greyColor.withOpacity(.1),
+            ),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(10),
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                AppStrings.schoolFeePayment,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 12.fontSize,
+                      color: AppColors.blackColor,
+                    ),
+              ),
+              AppSpacing.verticalSpaceSmall,
+              const Divider(
+                color: AppColors.greyColor,
+              ),
+              AppSpacing.verticalSpaceSmall,
+              ListTile(
+                contentPadding: EdgeInsets.zero,
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        SvgPicture.asset(
+                          'assets/svg/grad_cap.svg',
+                          color: AppColors.primaryColor,
+                        ),
+                        AppSpacing.horizontalSpaceSmall,
+                        Text(
+                          AppStrings.school,
+                          style:
+                              Theme.of(context).textTheme.bodySmall!.copyWith(
+                                    fontWeight: FontWeight.w300,
+                                    fontSize: 10.fontSize,
+                                    color: AppColors.greyColor,
+                                  ),
+                        ),
+                      ],
+                    ),
+                    AppSpacing.horizontalSpaceMassive,
+                    Row(
+                      children: [
+                        SvgPicture.asset(
+                          'assets/svg/star.svg',
+                          color: AppColors.primaryColor,
+                        ),
+                        AppSpacing.horizontalSpaceSmall,
+                        Text(
+                          AppStrings.classLevel,
+                          style:
+                              Theme.of(context).textTheme.bodySmall!.copyWith(
+                                    fontWeight: FontWeight.w300,
+                                    fontSize: 10.fontSize,
+                                    color: AppColors.greyColor,
+                                  ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                subtitle: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      constraints: BoxConstraints(maxWidth: 200.width),
+                      child: Text(
+                        'International School of Lagos, Akoka.',
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 12.fontSize,
+                              color: AppColors.primaryTextColor,
+                            ),
+                      ),
+                    ),
+                    Text(
+                      'Jss2',
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12.fontSize,
+                            color: AppColors.primaryTextColor,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+              AppSpacing.verticalSpaceSmall,
+              const Divider(
+                color: AppColors.greyColor,
+              ),
+              AppSpacing.verticalSpaceSmall,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'N 102,000',
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12.fontSize,
+                          color: AppColors.redColor,
+                        ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          color: AppColors.primaryColor,
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
+                      ),
+                      child: Text(
+                        AppStrings.payFee,
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12.fontSize,
+                              color: AppColors.primaryColor,
+                            ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        AppSpacing.verticalSpaceMedium,
+        Container(
+          padding: EdgeInsets.all(AppSpacing.horizontalSpacing),
+          decoration: BoxDecoration(
+            color: AppColors.primaryColor.withOpacity(0.1),
+            border: Border.all(
+              color: AppColors.greyColor.withOpacity(.1),
+            ),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(10),
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                AppStrings.schoolFeePayment,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 12.fontSize,
+                      color: AppColors.blackColor,
+                    ),
+              ),
+              AppSpacing.verticalSpaceSmall,
+              const Divider(
+                color: AppColors.greyColor,
+              ),
+              AppSpacing.verticalSpaceSmall,
+              ListTile(
+                contentPadding: EdgeInsets.zero,
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        SvgPicture.asset(
+                          'assets/svg/grad_cap.svg',
+                          color: AppColors.primaryColor,
+                        ),
+                        AppSpacing.horizontalSpaceSmall,
+                        Text(
+                          AppStrings.school,
+                          style:
+                              Theme.of(context).textTheme.bodySmall!.copyWith(
+                                    fontWeight: FontWeight.w300,
+                                    fontSize: 10.fontSize,
+                                    color: AppColors.greyColor,
+                                  ),
+                        ),
+                      ],
+                    ),
+                    AppSpacing.horizontalSpaceMassive,
+                    Row(
+                      children: [
+                        SvgPicture.asset(
+                          'assets/svg/star.svg',
+                          color: AppColors.primaryColor,
+                        ),
+                        AppSpacing.horizontalSpaceSmall,
+                        Text(
+                          AppStrings.classLevel,
+                          style:
+                              Theme.of(context).textTheme.bodySmall!.copyWith(
+                                    fontWeight: FontWeight.w300,
+                                    fontSize: 10.fontSize,
+                                    color: AppColors.greyColor,
+                                  ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                subtitle: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      constraints: BoxConstraints(maxWidth: 200.width),
+                      child: Text(
+                        'International School of Lagos, Akoka.',
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 12.fontSize,
+                              color: AppColors.primaryTextColor,
+                            ),
+                      ),
+                    ),
+                    Text(
+                      'Jss2',
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12.fontSize,
+                            color: AppColors.primaryTextColor,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+              AppSpacing.verticalSpaceSmall,
+              const Divider(
+                color: AppColors.greyColor,
+              ),
+              AppSpacing.verticalSpaceSmall,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'N 102,000',
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12.fontSize,
+                          color: AppColors.redColor,
+                        ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          color: AppColors.primaryColor,
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
+                      ),
+                      child: Text(
+                        AppStrings.payFee,
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12.fontSize,
+                              color: AppColors.primaryColor,
+                            ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
