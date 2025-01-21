48d07bad03f80a7b7be7250a92a42e4c63589008
Egundeyi Oladotun <Davidegundeyi@gmail.com>
2025-01-21 13:46:53 +0100

    feat: update home screen to use SplashScreen and add SchoolModel for authentication data

--- lib/src/features/authentication/api/models/auth_data_content.dart
+++ lib/src/features/authentication/api/models/auth_data_content.dart
@@ -1,3 +1,4 @@
+import 'package:edumake_frontend/src/features/authentication/api/models/school_models/school_model.dart';
 import 'package:edumake_frontend/src/features/authentication/api/models/user.dart';
 import 'package:json_annotation/json_annotation.dart';
 
@@ -8,13 +9,15 @@ class AuthDataContent {
   AuthDataContent({
     required this.user,
     required this.token,
+    this.school,
   });
 
   factory AuthDataContent.fromJson(Map<String, dynamic> json) =>
       _$AuthDataContentFromJson(json);
 
-  final User user;
   final String token;
+  final User user;
+  final SchoolModel? school;
 
   Map<String, dynamic> toJson() => _$AuthDataContentToJson(this);
 }
---
+++ lib/src/features/authentication/api/models/school_models/school_model.dart
@@ -0,0 +1,38 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'school_model.g.dart';
+
+@JsonSerializable()
+class SchoolModel {
+  SchoolModel(
+    this.admin,
+    this.schoolName,
+    this.schoolAddress,
+    this.schoolEmail,
+    this.schoolType,
+    this.classNumberRange,
+    this.studentNumberRange,
+    this.teacherNumberRange, {
+    this.id,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  factory SchoolModel.fromJson(Map<String, dynamic> json) =>
+      _$SchoolModelFromJson(json);
+
+  String? admin;
+  String? schoolName;
+  String? schoolAddress;
+  String? schoolEmail;
+  String? schoolType;
+  String? classNumberRange;
+  String? studentNumberRange;
+  String? teacherNumberRange;
+  @JsonKey(name: '_id')
+  String? id;
+  DateTime? createdAt;
+  DateTime? updatedAt;
+
+  Map<String, dynamic> toJson() => _$SchoolModelToJson(this);
+}
