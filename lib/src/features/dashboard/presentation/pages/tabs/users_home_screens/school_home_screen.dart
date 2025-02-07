9f12acf7746657bf83e8bf22c158ed48a6444374
Egundeyi Oladotun <Davidegundeyi@gmail.com>
2025-02-08 00:52:21 +0100

    feat: refactor SchoolDashBoard to use RequestsBloc for connection request management and enhance UI with loading and empty states

--- lib/src/features/dashboard/presentation/pages/tabs/users_home_screens/school_home_screen.dart
+++ lib/src/features/dashboard/presentation/pages/tabs/users_home_screens/school_home_screen.dart
@@ -3,14 +3,30 @@ import 'package:edumake_frontend/src/core/constants/app_spacing.dart';
 import 'package:edumake_frontend/src/core/constants/app_strings.dart';
 import 'package:edumake_frontend/src/core/extensions/num_extention.dart';
 import 'package:edumake_frontend/src/features/authentication/presentation/pages/school/connection_request_screen.dart';
+import 'package:edumake_frontend/src/features/dashboard/presentation/bloc/requests/requests_bloc.dart';
 import 'package:edumake_frontend/src/features/dashboard/presentation/widgets/connection_request_list_tile.dart';
 import 'package:edumake_frontend/src/features/dashboard/presentation/widgets/recent_teachers_note.dart';
 import 'package:edumake_frontend/src/features/dashboard/presentation/widgets/school_mgt_upcoming_events_container.dart';
 import 'package:edumake_frontend/src/shared/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:formz/formz.dart';
 
-class SchoolDashBoard extends StatelessWidget {
+class SchoolDashBoard extends StatefulWidget {
   const SchoolDashBoard({super.key});
+
+  @override
+  State<SchoolDashBoard> createState() => _SchoolDashBoardState();
+}
+
+class _SchoolDashBoardState extends State<SchoolDashBoard> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<RequestsBloc>().add(const RequestsEvent.getRequest());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -55,68 +71,131 @@ class SchoolDashBoard extends StatelessWidget {
           ),
         ),
         AppSpacing.verticalSpaceMedium,
-        Align(
-          alignment: Alignment.topLeft,
-          child: GestureDetector(
-            onTap: () {},
-            child: Text(
-              '${AppStrings.connectionRequest} (10)',
-              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                    fontSize: 16.fontSize,
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.blackColor,
-                  ),
-            ),
-          ),
-        ),
-        AppSpacing.verticalSpaceMedium,
-        Container(
-          width: double.infinity,
-          padding: EdgeInsets.only(
-            top: AppSpacing.verticalValueSmall,
-            left: AppSpacing.horizontalSpacing,
-            right: AppSpacing.horizontalSpacing,
-          ),
-          decoration: BoxDecoration(
-            color: AppColors.primaryColor.withOpacity(0.1),
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: ListView.separated(
-            itemCount: 5,
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              return const ConnectionRequestListTile(
-                titleName: 'Kamala Harris',
-                subTitleName: 'Donald Trump',
-                profilePic: null,
-                date: 'Today 4:20',
-                className: 'JSS 1',
+        BlocBuilder<RequestsBloc, RequestsState>(
+          builder: (context, state) {
+            if (state.getRequestStatus == FormzSubmissionStatus.inProgress) {
+              return const Center(
+                child: SpinKitPulsingGrid(
+                  color: AppColors.primaryColor,
+                  size: 30,
+                ),
               );
-            },
-            separatorBuilder: (BuildContext context, int index) {
-              return const Divider(
-                color: AppColors.greyColor,
-                thickness: 1,
-              );
-            },
-          ),
-        ),
-        AppSpacing.verticalSpaceMedium,
-        Align(
-          alignment: Alignment.bottomRight,
-          child: GestureDetector(
-            onTap: () => Navigator.of(context, rootNavigator: true)
-                .pushNamed(ConnectionRequestScreen.routeName),
-            child: Text(
-              AppStrings.seeAll,
-              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    color: AppColors.primaryColor,
-                    fontSize: 16.fontSize,
-                    fontWeight: FontWeight.w700,
+            }
+            if (state.getRequestModel?.data == null ||
+                state.getRequestModel!.data.isEmpty) {
+              return Padding(
+                padding: const EdgeInsets.all(8),
+                child: Center(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: AppSpacing.verticalValueSpaceLarge * 8,
+                      ),
+                      Image.asset(
+                        'assets/png/empty.png',
+                        height: 150,
+                      ),
+                      Text(
+                        'No Data Available',
+                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                              fontSize: 20, // Assuming 20 is a valid font size
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.primaryTextColor,
+                            ),
+                      ),
+                      AppSpacing.verticalSpaceSmall,
+                      Text(
+                        'Your connection requests will appear here once they are available. Please check back later or contact support if you believe this is an error.',
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                              fontSize: 14, // Assuming 14 is a valid font size
+                              fontWeight: FontWeight.w400,
+                              color: AppColors.secondaryTexColor,
+                            ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
                   ),
-            ),
-          ),
+                ),
+              );
+            }
+            return Column(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    '${AppStrings.connectionRequest} (${context.read<RequestsBloc>().state.getRequestModel?.data.length ?? 0})',
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          fontSize: 16.fontSize,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.blackColor,
+                        ),
+                  ),
+                ),
+                AppSpacing.verticalSpaceMedium,
+                BlocBuilder<RequestsBloc, RequestsState>(
+                  builder: (context, state) {
+                    return Container(
+                      width: double.infinity,
+                      padding: EdgeInsets.only(
+                        top: AppSpacing.verticalValueSmall,
+                        left: AppSpacing.horizontalSpacing,
+                        right: AppSpacing.horizontalSpacing,
+                      ),
+                      decoration: BoxDecoration(
+                        color: AppColors.primaryColor.withOpacity(0.1),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: ListView.separated(
+                        itemCount: state.getRequestModel?.data.length ?? 0,
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          final request = state.getRequestModel!.data[index];
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.of(context, rootNavigator: true)
+                                  .pushNamed(
+                                ConnectionRequestDetailsScreen.routeName,
+                                arguments: request.id,
+                              );
+                            },
+                            child: ConnectionRequestListTile(
+                              titleName: "Parent's name",
+                              subTitleName: request.student.name,
+                              date: request.createdAt.toString(),
+                              className: 'Class name',
+                            ),
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) {
+                          return const Divider(
+                            color: AppColors.greyColor,
+                            thickness: 1,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+                AppSpacing.verticalSpaceMedium,
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: GestureDetector(
+                    onTap: () => Navigator.of(context, rootNavigator: true)
+                        .pushNamed(ConnectionRequestScreen.routeName),
+                    child: Text(
+                      AppStrings.seeAll,
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: AppColors.primaryColor,
+                            fontSize: 16.fontSize,
+                            fontWeight: FontWeight.w700,
+                          ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
         ),
         AppSpacing.verticalSpaceMedium,
         Align(
