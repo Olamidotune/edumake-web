7b4c8896ff91759917446c9810e18b4acc522388
Egundeyi Oladotun <Davidegundeyi@gmail.com>
2025-02-07 17:26:01 +0100

    feat: update dashboard and school classes screens to manage school data fetching

--- lib/src/features/dashboard/presentation/pages/tabs/users_payment_screens/school_tab/school_classes_screen.dart
+++ lib/src/features/dashboard/presentation/pages/tabs/users_payment_screens/school_tab/school_classes_screen.dart
@@ -30,6 +30,9 @@ class _ClassScreenState extends State<ClassScreen> {
   void initState() {
     super.initState();
     scrollController.addListener(_loadMoreClasses);
+    context
+        .read<GetSchoolDataBloc>()
+        .add(const GetSchoolDataEvent.fetchPaginatedClasses());
   }
 
   @override
